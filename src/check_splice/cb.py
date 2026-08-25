27305cc577f8fc8d52db169e21d9578b63a083a3
ljw <ljw2017@sjtu.edu.cn>
2026-08-25 17:17:19 +0800

    Use coolbox to display genes.

--- src/check_splice/cb.py
+++ src/check_splice/cb.py
@@ -1,10 +1,18 @@
 import os
 
 import matplotlib.pyplot as plt
+import pypdf
 from coolbox.api import *
 
 
-def draw_links(cfg: dict, pairs_file: os.PathLike):
+def draw_links(cfg: dict, exp_protein_wt: str, orientation: str):
+    if exp_protein_wt:
+        pairs_file = (
+            cfg["data_dir"] / "result" / "hic" / f"{exp_protein_wt}_{orientation}.pairs"
+        )
+    else:
+        pairs_file = cfg["data_dir"] / "result" / "hic" / f"{orientation}.pairs"
+
     regions = {
         "cpcdh": {
             "chrom": cfg["chrom"],
@@ -19,12 +27,36 @@ def draw_links(cfg: dict, pairs_file: os.PathLike):
         frame = (
             XAxis()
             + Pairs(os.fspath(pairs_file))
+            + TrackHeight(5)
             + Title("splice")
             + BED(os.fspath(cfg["data_dir"] / "data" / "hg19.12.bed"))
             + Title("gene")
             + BED(os.fspath(cfg["data_dir"] / "result" / "pCBS.bed"))
             + Title("pCBS")
+            + FrameTitle(f"{exp_protein_wt}:{orientation}:{cluster}")
         )
         fig = frame.plot(f"{region['chrom']}:{region['start']}-{region['end']}")
         fig.savefig(os.fspath(pairs_file.with_suffix(f".{cluster}.pdf")))
         plt.close(fig)
+
+        yield pairs_file.with_suffix(f".{cluster}.pdf")
+
+
+def draw_links_all(cfg):
+    pdf_files = []
+    with pypdf.PdfWriter() as pdf_writer:
+        for orientation in ["ff", "rr"]:
+            for pdf_file in draw_links(cfg, "", orientation):
+                pdf_writer.append(pdf_file)
+                pdf_files.append(pdf_file)
+            with open("exp_protein_wts.txt", "r") as fd:
+                for exp_protein_wt in fd:
+                    exp_protein_wt = exp_protein_wt.strip()
+                    for pdf_file in draw_links(cfg, exp_protein_wt, orientation):
+                        pdf_writer.append(pdf_file)
+                        pdf_files.append(pdf_file)
+
+        pdf_writer.write(cfg["data_dir"] / "result" / "hic" / "links.pdf")
+
+    for pdf_file in pdf_files:
+        pdf_file.unlink()
